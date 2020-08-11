5b9e9d645bbc555c07acd82541de8c4330d8fc0d
freefirebot <69522801+freefirebot@users.noreply.github.com>
2020-08-11 21:04:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyB7qLx6yyccHsxOV1UcliCBTkUoyAn-kRQ",
+  authDomain: "free-fire-3b268.firebaseapp.com",
+  databaseURL: "https://free-fire-3b268.firebaseio.com",
+  projectId: "free-fire-3b268",
+  storageBucket: "free-fire-3b268.appspot.com",
+  messagingSenderId: "600329187689",
+  appId: "1:600329187689:web:94c9508c439c0daf5ecdf8",
+  measurementId: "G-NMRBWLKXYW"
 };
 
      
